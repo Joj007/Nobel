c98ff9a0eb97cf3bf25bade2404ea23d7583968f
Mechwart András <mechwwart@gmail.com>
2023-06-01 10:53:52 +0200

    Kész a kötelező

---
+++ nobel/Nobel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nobel
+{
+    
+    class Nobel
+    {
+        int ev;
+        string tipus;
+        string keresztNev;
+        string vezetekNev;
+
+        public Nobel(string sor)
+        {
+            string[] mezok = sor.Split(';');
+            ev = int.Parse(mezok[0]);
+            tipus = mezok[1];
+            keresztNev = mezok[2];
+            vezetekNev = mezok[3];
+        }
+
+        public Nobel(int ev, string tipus, string keresztNev, string vezetekNev)
+        {
+            this.ev = ev;
+            this.tipus = tipus;
+            this.keresztNev = keresztNev;
+            this.vezetekNev = vezetekNev;
+        }
+
+        public int Ev { get => ev;}
+        public string Tipus { get => tipus;}
+        public string KeresztNev { get => keresztNev;}
+        public string VezetekNev { get => vezetekNev;}
+        public string TeljesNev { get => keresztNev + " " + vezetekNev; }
+        public string Fajlba { get => $"{Ev}:{TeljesNev}"; }
+    }
+}
--- nobel/Program.cs
+++ nobel/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace nobel
 {
@@ -10,6 +11,27 @@ namespace nobel
     {
         static void Main(string[] args)
         {
+            //2
+            List<Nobel> nobelLista = new List<Nobel>();
+            foreach (var sor in File.ReadAllLines("nobel.csv").Skip(1))
+            {
+                nobelLista.Add(new Nobel(sor));
+            }
+
+            //4
+            Console.WriteLine($"2017 irodalmi nobelíjasa: {nobelLista.Where(n => n.Ev == 2017 && n.Tipus == "irodalmi").First().TeljesNev}");
+
+            //5
+            //nobelLista.Where(n => n.VezetekNev == "" && n.Ev >= 1990).ToList().ForEach();
+
+            //6
+            nobelLista.Where(n => n.VezetekNev.Contains("Curie")).ToList().ForEach(n => Console.WriteLine($"Év: {n.Ev}, Név: {n.TeljesNev}, Kategória: {n.Tipus}"));
+
+            //7
+            nobelLista.GroupBy(n => n.Tipus).ToList().ForEach(n => Console.WriteLine($"{n.Key}: {n.Count()}"));
+
+            //8
+            File.WriteAllLines("orvosi.txt", nobelLista.Where(n=>n.Tipus=="orvosi").OrderBy(n=>n.Ev).Select(n=>n.Fajlba));
         }
     }
 }
